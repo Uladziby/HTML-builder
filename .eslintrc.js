5a01aa4e0000a2e28b93da0cd4996e18ed5d7595
Vlad Yanushevsky <yanushevskyv@gmail.com>
2021-11-08 15:26:12 +0300

    feat  four  task

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ],
     'quotes': [
       'error',
-      'single'
+      'duoble'
     ],
     'semi': [
       'error',
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs/promises");
+
+const filePath = path.join(__dirname, "files");
+const filesCopyPath = path.join(__dirname, "files-copy");
+
+async function getOutFolder() {
+  fsPromises
+    .readdir(filesCopyPath)
+    .then((files) => {
+      files.map((file) => {
+        fsPromises.rm(filesCopyPath + `/${file}`);
+      });
+    })
+    .catch(() => {
+      console.log("no such directory");
+    })
+    .finally(() => {
+      createFolder();
+    });
+}
+
+function createFolder() {
+  console.log("create folder");
+  fsPromises
+    .mkdir(filesCopyPath)
+    .catch((err) => {
+      console.log("Folder is exist");
+    })
+    .finally(() => {
+      copyToFolder();
+    });
+}
+
+function copyToFolder() {
+  console.log("copyToFile");
+  fsPromises
+  .readdir(filePath)
+  .then((files) => {
+    files.map((file) => {
+      console.log("copy ", file, `to files-copy`);
+      fsPromises
+      .copyFile(
+        filePath + `/${file}`,
+        filesCopyPath + `/${file}`
+      );
+    });
+  });
+}
+
+getOutFolder();
 
