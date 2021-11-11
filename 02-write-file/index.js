808d31f62f6e514ab29c44e2d16d12ce24d68bb6
Vlad Yanushevsky <yanushevskyv@gmail.com>
2021-11-11 12:12:04 +0300

    fix second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,28 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { stdout , stdin} = require("process");
 
+const filePath = path.join(__dirname, "out.txt");
+console.log("Hey, write your text");
+const ws = fs.createWriteStream(filePath);
 
-const filePath = path.join(__dirname, 'out.txt');
-fs.writeFile(filePath,'',()=>{});
-console.log('Hey, write your text');
-
-process.stdin.on('data', (data)=>{
-  if(data.toString().split('\n')[0]==='exit'){
+stdin.on("data", (data) => {
+  if (data.toString().split("\n")[0] === "exit") {
     process.exit();
-
+  } else {
+    ws.write(data);
+    console.log("--something else?--");
   }
-  fs.readFile(filePath,(err, data1)=>{
-    fs.writeFile(filePath, `${data1}${data}` , (err)=>{
-      if(err){
-        throw err;
-      }
-    });
- 
-    console.log('--something else?--');
-  });
 });
 
-process.on("exit", () => process.stdout.write('--Bye bye--'));
+process.on("exit", () => stdout.write("--Bye bye--"));
 process.on("SIGINT", () => {
   process.exit();
 });
