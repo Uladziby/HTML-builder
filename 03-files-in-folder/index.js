c876afa4f4200629b1b205b561dc4eedf0364049
Vlad Yanushevsky <yanushevskyv@gmail.com>
2021-11-11 10:19:13 +0300

    fix second and third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,7 +7,7 @@ const filePath = `${__dirname}/secret-folder`;
 fs.readdir(filePath, { withFileTypes: true }, (err, files) => {
   if (err) throw err;
   files.map((file) => {
-    if (file.isFile) {
+    if (file.isFile()) {
       stat(path.join(__dirname, "secret-folder", file.name), (err, stats) => {
         const name = file.name.split(".");
         console.log(
